4b84b22d2b374229da671a22063dad8f6f7dbc13
Victor Porton <porton.victor@gmail.com>
2022-12-30 14:11:15 +0200

    fixes in tests

--- test/Lock.ts
+++ test/Lock.ts
@@ -4,7 +4,7 @@ import "@nomiclabs/hardhat-ethers";
 import { expect } from "chai";
 import { ethers } from "hardhat";
 
-describe("Lock", function () {
+describe("Crowdfund", function () {
   // We define a fixture to reuse the same setup in every test.
   // We use loadFixture to run this setup once, snapshot that state,
   // and reset Hardhat Network to that snapshot in every test.
